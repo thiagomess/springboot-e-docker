6428911120178ac72a96971ec169b70781bef393
Thiago Gomes <thiagogomes19@hotmail.com>
2020-01-31 18:08:57 -0300

    usando FLYWAY para migração de BD

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Sao Paulo', 'Person name 0', 'male', 'Last name'),
+	(2, 'Sao Paulo', 'Person name 0', 'male', 'Last name'),
+	(3, 'Sao Paulo', 'Person name 0', 'male', 'Last name'),
+	(4, 'Sao Paulo', 'Person name 0', 'male', 'Last name');
